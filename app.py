36b3f31b5f62be7954baafe89334314bc2750ac7
Kevin Grogan <kevin.grogan@omni.sc>
2020-03-29 13:00:56 -0700

    added db

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from model import *
 
 @app.route('/')
 def hello():
@@ -12,6 +17,7 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return f"Hello {name}!"
+
 
 if __name__ == '__main__':
     app.run()
