95a78921ca0ee3aa96be48acaf0988a0813f9d1d
Kevin Grogan <kevin.grogan@omni.sc>
2020-12-25 11:09:06 -0800

    Reformated exercises.json and deleted unused file.

--- src/excercise_database.py
+++ src/excercise_database.py
@@ -140,4 +140,4 @@ if __name__ == '__main__':
         compressed_exercises = yaml.load(exercise_file)
         raw_exercises = decompress_exercises(compressed_exercises)
         df = create_dataframe(raw_exercises)
-        df.to_json(EXCERCISE_DATAFRAME, orient="index", indent=5)
+        df.to_json(EXCERCISE_DATAFRAME, orient="records", indent=5)
