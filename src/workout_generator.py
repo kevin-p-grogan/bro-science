dab281d67b16f291d7394ec0744c8d19febaa8a3
Kevin Grogan <kevin.grogan@omni.sc>
2021-06-26 07:53:40 -0700

    Updated exercises.

--- src/workout_generator.py
+++ src/workout_generator.py
@@ -16,14 +16,16 @@ EXERCISE_CACHE = 'data/exercise_tmp.pkl'
 INHERITANCE_KEYWORD = 'inherit'
 COMPLETE_WORKOUT_KEYWORD = 'Complete'
 
+
 def create_workouts_json(filename):
     workouts = load_workouts()
     with open(filename, 'w') as f:
         json.dump(workouts, f, indent=5)
 
 
-def __deep_merge_dictionaries(a, b, path=None):
-    "merges b into a. Adapted from https://stackoverflow.com/questions/7204805/how-to-merge-dictionaries-of-dictionaries/7205107#7205107 "
+def _deep_merge_dictionaries(a, b, path=None):
+    """merges b into a. Adapted from
+    https://stackoverflow.com/questions/7204805/how-to-merge-dictionaries-of-dictionaries/7205107#7205107"""
 
     if path is None:
         path = []
@@ -31,7 +33,7 @@ def __deep_merge_dictionaries(a, b, path=None):
     for key in b:
         if key in a:
             if isinstance(a[key], dict) and isinstance(b[key], dict):
-                __deep_merge_dictionaries(a[key], b[key], path + [str(key)])
+                _deep_merge_dictionaries(a[key], b[key], path + [str(key)])
             else:
                 a[key] = b[key]
         else:
@@ -40,16 +42,16 @@ def __deep_merge_dictionaries(a, b, path=None):
     return a
 
 
-def __resolve_inheritance(workouts, root):
+def _resolve_inheritance(workouts, root):
     if not isinstance(workouts, dict):
         return
 
     merged_workouts = workouts
     for k, v in workouts.items():
-        __resolve_inheritance(v, root)
+        _resolve_inheritance(v, root)
         if k == INHERITANCE_KEYWORD:
             inherited = deepcopy(root[workouts[k]])
-            merged_workouts = __deep_merge_dictionaries(inherited, workouts)
+            merged_workouts = _deep_merge_dictionaries(inherited, workouts)
 
     for k, v in merged_workouts.items():
         if k == INHERITANCE_KEYWORD:
@@ -58,7 +60,7 @@ def __resolve_inheritance(workouts, root):
             workouts[k] = v
 
 
-def __load_workout(workout_name):
+def _load_workout(workout_name):
     # load the raw workout
     workouts = load_workouts()
     return workouts[workout_name]
@@ -67,12 +69,12 @@ def __load_workout(workout_name):
 def load_workouts():
     with open(WORKOUTS_FILENAME, 'r') as workouts_file:
         workouts = yaml.load(workouts_file)
-        __resolve_inheritance(workouts, workouts)
-        workouts = __filter_partial_workouts(workouts)
+        _resolve_inheritance(workouts, workouts)
+        workouts = _filter_partial_workouts(workouts)
     return workouts
 
 
-def __filter_partial_workouts(workouts):
+def _filter_partial_workouts(workouts):
     filtered_workouts = dict()
     for k, v in workouts.items():
         if v.get(COMPLETE_WORKOUT_KEYWORD, False):
@@ -82,7 +84,7 @@ def __filter_partial_workouts(workouts):
     return filtered_workouts
 
 
-def __apply_filters(df):
+def _apply_filters(df):
     df = df.copy()
     with open(FILTERS_FILENAME, 'r') as filters_file:
         filters = yaml.load(filters_file)
@@ -102,7 +104,7 @@ def __apply_filters(df):
     return df
 
 
-def __select_exercises(workout, exercise_dataframe):
+def _select_exercises(workout, exercise_dataframe):
     dfs = []
     for exercise in workout:
         df = exercise_dataframe.copy()
@@ -120,7 +122,7 @@ def __select_exercises(workout, exercise_dataframe):
     return df.sort_values(by='order')
 
 
-def __convert_to_template(exercises):
+def _convert_to_template(exercises):
     template = []
     for _, row in exercises.iterrows():
         template.append({
@@ -133,7 +135,7 @@ def __convert_to_template(exercises):
 
 
 #TODO: Change how the tracking of exercises is handled. This is not scalable to more than one user.
-def __load_exercises():
+def _load_exercises():
     """Load the temporary file of exercises"""
     with open(EXERCISE_CACHE, 'rb') as f:
         exercises = pkl.load(f)
@@ -141,16 +143,16 @@ def __load_exercises():
     return exercises
 
 
-def __save_exercises(exercises):
+def _save_exercises(exercises):
     """Save exercises out to temporary file"""
     with open(EXERCISE_CACHE, 'wb') as f:
         pkl.dump(exercises, f)
 
 
 def resample_exercise(exercise):
-    exercises = __load_exercises()
+    exercises = _load_exercises()
     exercise_dataframe = pd.read_json(EXCERCISE_DATAFRAME)
-    exercise_dataframe = __apply_filters(exercise_dataframe)
+    exercise_dataframe = _apply_filters(exercise_dataframe)
 
     old_exercise = exercise_dataframe[exercise_dataframe.name == exercise]
     direction_and_group = old_exercise.direction_and_group.iloc[0]
@@ -162,18 +164,19 @@ def resample_exercise(exercise):
     new_exercise = df.sample(n=1, weights=df.rating, random_state=datetime.now().microsecond).name.iloc[0]
     exercises.name[exercises.name == exercise] = new_exercise
 
-    __save_exercises(exercises)
-    return __convert_to_template(exercises)
+    _save_exercises(exercises)
+    return _convert_to_template(exercises)
 
 
 def generate_workout(workout_name, week):
     exercise_dataframe = pd.read_json(EXCERCISE_DATAFRAME)
-    exercise_dataframe = __apply_filters(exercise_dataframe)
+    exercise_dataframe = _apply_filters(exercise_dataframe)
     full_workout_name = ' '.join((workout_name, week))
-    workout = __load_workout(full_workout_name)
-    exercises = __select_exercises(workout, exercise_dataframe)
-    __save_exercises(exercises)
-    return __convert_to_template(exercises)
+    workout = _load_workout(full_workout_name)
+    exercises = _select_exercises(workout, exercise_dataframe)
+    _save_exercises(exercises)
+    return _convert_to_template(exercises)
+
 
 if __name__ == "__main__":
     #  Generate and save the workout.
